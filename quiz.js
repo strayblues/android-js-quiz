c8d618f3997502bc044a143a9cac0ab9c67c72e0
Hagar Shilo <strayblues@gmail.com>
2016-10-22 18:17:46 +0300

    Start documenting

--- quiz.js
+++ quiz.js
@@ -8,16 +8,23 @@ $.ajaxSetup({
     async: false
 });
 
+//Where we'll store the correct answers and the answers picked by the user
 var correctAnswers = [];
 var userAnswers = new Array(10);
 
+//Calculate and show the score.
+//The user gets a point for each "user answer" that matches a "correct answer"
 function calcScore(){
     for(var score=0, i=0; i<10; i++){
     if (correctAnswers[i] === userAnswers[i]){
         score++;
         }
     }
+    //The Show Score button should disappear at this point
     $("#bt_score").hide();
+    //The Back button should -maybe- disappear at this point
+    $("#bt_prev").hide();
+    //Show the calculated score
     $("#score").text("YOUR SCORE: "+score);
 }
 
@@ -51,15 +58,17 @@ $.getJSON("quiz.json", function (content){
 });
 
 function showQuestion(){
-    //guarantee all answers are unchecked
+    //Make sure all answers are unchecked when a new question is displayed
     $("#answer1button").prop("checked", false);
     $("#answer2button").prop("checked", false);
     $("#answer3button").prop("checked", false);
     $("#answer4button").prop("checked", false);
 
+    //What is this variable?
     var q = questionList[clickCount];
     $('#question').text(q['question']);
 
+    //What's happening?
     var answerz = [
         q['correct answer'],
         q['wrong answers'][0],
@@ -67,27 +76,37 @@ function showQuestion(){
         q['wrong answers'][2]
     ];
 
+        //Suffle the answers
         shuffle(answerz);
 
+        //Set the questions' order by inserting the shuffled answers into each
+        //of the 4 places
         $('#answer1').text(answerz[0]);
         $('#answer2').text(answerz[1]);
         $('#answer3').text(answerz[2]);
         $('#answer4').text(answerz[3]);
 }
 
+//When the session starts, the counter is set to 0
 var clickCount = 0;
 showQuestion();
 $("#bt_next").show();
+//When the 1st question is displayed, the Back button is hidden
 $("#bt_prev").hide();
+//The Score button is hidden as well
 $("#bt_score").hide();
 
+//The counter tacks the user's steps forward and back
 $("#bt_next").click(function(){
     clickCount+=1;
     showQuestion();
+    //When counter>0 that means we're past the fisrt question,
+    //and therefore the Back/previous button should be displayed
     $("#bt_prev").show();
     $("#bt_next").show();
     $("#bt_score").hide();
-
+    //after 10 questions, the next button disappears
+    //and the score button is shown
     if (clickCount === 9) {
     $("#bt_score").show();
     $("#bt_next").hide();
@@ -110,7 +129,7 @@ $("#bt_prev").click(function(){
 );
 
 var currentQuestion = 0;
-
+//What's going on here?
 $('#answer1button').click(function() {
     userAnswers[clickCount] = ($('#answer1').text());
 });
