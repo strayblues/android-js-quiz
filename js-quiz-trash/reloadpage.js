e588ce3505757f798e525f1fb780d53cf5b6e89c
Hagar Shilo <strayblues@gmail.com>
2016-10-26 02:31:23 +0300

    Remove redundant files and remove bad questions from JSON

--- js-quiz-trash/reloadpage.js
+++
@@ -1,8 +0,0 @@
-function reloadPage(){
-window.location = window.location.href+"reload=true";}
-
-function checkQString(){
-// Get query strings and store reload qstring in variable
-if (reload) {
-    document.getElementById('bt_again').innerHTML = 'Try Again!';
-}
